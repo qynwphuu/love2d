6b0a3f570a9ba047faf32f2fdb3e17ab75e5e1ce
qynwphuu <qynwphuu@gmail.com>
2026-05-16 14:45:29 +0300

    speed

--- main.lua
+++ main.lua
@@ -2,23 +2,24 @@ function love.load()
 	player = {}
 	player.x = 400
 	player.y = 200
+	player.speed = 1
 end
 
 function love.update(dt)
 	if love.keyboard.isDown("right") then
-		player.x = player.x + 1
+		player.x = player.x + player.speed
 	end
 
 	if love.keyboard.isDown("left") then
-		player.x = player.x - 1
+		player.x = player.x - player.speed
 	end
 
 	if love.keyboard.isDown("down") then
-		player.y = player.y + 1
+		player.y = player.y + player.speed
 	end
 
 	if love.keyboard.isDown("up") then
-		player.y = player.y - 1
+		player.y = player.y - player.speed
 	end
 end
 
