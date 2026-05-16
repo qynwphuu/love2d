53388132855ef3e2a6fd15462081e34fc0d638b0
qynwphuu <qynwphuu@gmail.com>
2026-05-16 14:35:41 +0300

    start

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+	player = {}
+	player.x = 400
+	player.y = 200
+end
+
+function love.update(dt) end
+
+function love.draw()
+	love.graphics.circle("fill", player.x, player.y, 100)
+end
